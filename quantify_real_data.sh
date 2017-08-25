59805ee897801301712f69366cbb924a7ae882d8
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-08-25 09:22:54 +0100

    Update quantify_real_data.sh

--- quantify_real_data.sh
+++ quantify_real_data.sh
@@ -6,7 +6,7 @@ Kallisto() {
       filename=$1
       mkdir Simulation/Kallisto_results_real_data/$filename
 
-      ./Simulation/kallisto_linux-v0.43.0/kallisto quant -i Simulation/indices/Kallisto/transcripts.idx --threads=8 --output-dir=Simulation/Kallisto_results_real_data/$filename $TEAM/BLUEPRINT_prac/$filename'_1.fq.gz' $TEAM/BLUEPRINT_prac/$filename'_2.fq.gz'
+      ./Simulation/kallisto_linux-v0.43.1/kallisto quant -i Simulation/indices/Kallisto/transcripts.idx --threads=8 --output-dir=Simulation/Kallisto_results_real_data/$filename $TEAM/BLUEPRINT_prac/$filename'_1.fq.gz' $TEAM/BLUEPRINT_prac/$filename'_2.fq.gz'
 
       echo "target_id       length  eff_length      est_counts      tpm" >> Simulation/Kallisto_results_real_data/$filename/abundancesorted.tsv
       tail -n +2 Simulation/Kallisto_results_real_data/$filename/abundance.tsv | sort -n -k1.8 >> Simulation/Kallisto_results_real_data/$filename/abundancesorted.tsv
