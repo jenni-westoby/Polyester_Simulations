8977892ad3c90cb2b244a17b3126710ef528e217
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-09-01 14:17:45 +0100

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -65,7 +65,7 @@ benchmark(){
         for i in $(find . -name '*_1.fq');
         do
           base=`echo $i |awk -F/ '{print $2}'`
-          filename=`echo $base |awk -F_ '{print $1}'`
+          filename=`echo $base |awk -F_ '{print $1"_"$2}'`
           cd $memory
           #The line below will need to be edited for your LSF job system.
           bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/"output."$filename$1 -e $TEAM/temp.logs/"error."$filename$1 -R"select[mem>100000] rusage[mem=100000]" -M 100000 ./quantify.sh $1 $filename
