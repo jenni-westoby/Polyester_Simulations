c54a0615950e4723c25867df87551e2846524135
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-09-01 13:56:01 +0100

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -62,13 +62,13 @@ benchmark(){
 
         #Run tool on simulated cells. Each cell is submitted as a seperate job.
         cd Simulation/data/simulated
-        for i in $(find . -name '*_1.fasta*');
+        for i in $(find . -name '*_1.fq');
         do
           base=`echo $i |awk -F/ '{print $2}'`
           filename=`echo $base |awk -F_ '{print $1}'`
           cd $memory
           #The line below will need to be edited for your LSF job system.
-          bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/"output."$filename$1 -e $TEAM/temp.logs/"error."$filename$1 -R"select[mem>200000] rusage[mem=200000]" -M 200000 ./quantify.sh $1 $filename
+          bsub -n8 -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/"output."$filename$1 -e $TEAM/temp.logs/"error."$filename$1 -R"select[mem>100000] rusage[mem=100000]" -M 100000 ./quantify.sh $1 $filename
         done
 
         #If a data matrix hasn't been made for the ground_truth results, make it
